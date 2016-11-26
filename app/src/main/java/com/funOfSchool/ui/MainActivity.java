a20b780ae04d4f8d13ff98ae5bbb39335af50f9b
tobewithMayday <405841911@qq.com>
2016-11-26 21:10:44 +0800

    注册高德地图

--- app/src/main/java/com/funOfSchool/ui/MainActivity.java
+++ app/src/main/java/com/funOfSchool/ui/MainActivity.java
@@ -3,6 +3,8 @@ package com.funOfSchool.ui;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.amap.api.location.AMapLocationClient;
+import com.amap.api.location.AMapLocationListener;
 import com.funOfSchool.R;
 
 public class MainActivity extends AppCompatActivity {
