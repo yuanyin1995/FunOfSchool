90069b4d88ffa2967e2f53da408de4a931240be2
tobewithMayday <405841911@qq.com>
2016-11-27 20:43:08 +0800

    配置地图

--- app/src/main/java/com/funOfSchool/ui/MainActivity.java
+++ app/src/main/java/com/funOfSchool/ui/MainActivity.java
@@ -3,15 +3,46 @@ package com.funOfSchool.ui;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import com.amap.api.location.AMapLocationClient;
-import com.amap.api.location.AMapLocationListener;
+import com.amap.api.maps2d.MapView;
 import com.funOfSchool.R;
 
 public class MainActivity extends AppCompatActivity {
+    MapView mMapView = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //获取地图控件引用
+        mMapView = (MapView) findViewById(R.id.amap_view);
+        //在activity执行onCreate时执行mMapView.onCreate(savedInstanceState)，实现地图生命周期管理
+        mMapView.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        //在activity执行onDestroy时执行mMapView.onDestroy()，实现地图生命周期管理
+        mMapView.onDestroy();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //在activity执行onResume时执行mMapView.onResume ()，实现地图生命周期管理
+        mMapView.onResume();
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //在activity执行onPause时执行mMapView.onPause ()，实现地图生命周期管理
+        mMapView.onPause();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        //在activity执行onSaveInstanceState时执行mMapView.onSaveInstanceState (outState)，实现地图生命周期管理
+        mMapView.onSaveInstanceState(outState);
     }
 }
