bcf214adffe35012674741d581ebf70037805d08
yuanyin1995 <13199980@qq.com>
2016-12-10 21:19:38 +0800

    个人资料界面 补充 + 出游列表界面

--- app/src/main/java/com/funOfSchool/ui/PersonInfoActivity.java
+++ app/src/main/java/com/funOfSchool/ui/PersonInfoActivity.java
@@ -8,6 +8,7 @@ import android.support.v4.widget.DrawerLayout;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.funOfSchool.R;
@@ -17,6 +18,7 @@ import com.funOfSchool.R;
  */
 public class PersonInfoActivity extends Activity {
     private ImageButton btnBack;
+    private TextView tvName;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_personinfo);
@@ -24,13 +26,19 @@ public class PersonInfoActivity extends Activity {
         findView();
         //  为各按钮设置监听器
         setListener();
+        // 为各资料项设值
+        setInfoValue();
     }
     private void findView(){
         btnBack = (ImageButton)findViewById(R.id.back);
+        tvName = (TextView)findViewById(R.id.value_name);
     }
     private void setListener(){
         PersoninfoListener personinfoListener = new PersoninfoListener();
         btnBack.setOnClickListener(personinfoListener);
+    }
+    private void setInfoValue(){
+        tvName.setText("音袁");
     }
     private class PersoninfoListener implements View.OnClickListener{
         @Override
